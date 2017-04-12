9c067b5af79d53db64f07cabe2172df2146277fc
Martin <m.todorov94@gmail.com>
2017-04-12 21:49:22 +0300

    adding half of the Blog

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+namespace ASP.NET_Blog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ASP.NET_Blog.Models.BlogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(ASP.NET_Blog.Models.BlogDbContext context)
+        {
+            
+        }
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace ASP.NET_Blog.Models
+{
+    public class Article
+    {
+        public Article()
+        {
+            this.Date = DateTime.Now;
+        }
+        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
